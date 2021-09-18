26c3a4bb6c6f16f85315d1e390dbdc360931abc5
ErickEzrandy274 <75024713+ErickEzrandy274@users.noreply.github.com>
2021-09-18 19:29:45 +0700

    Change date on myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# REV00 Sun 12 Sep 2021 03:10:00 WIB
-# START Sun 12 Sep 2021 03:10:00 WIB
+# REV00 Wed 15 Sep 2021 21:30:00 WIB
+# START Wed 15 Sep 2021 21:30:00 WIB
 
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
